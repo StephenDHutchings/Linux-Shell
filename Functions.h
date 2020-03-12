3b192f95ce3803c2525d7a2c91b539d24967b5e1
Mohamed Abdel-Hafiz <mohamed.abdel-hafiz@ucdenver.edu>
2020-03-12 12:05:28 -0600

    added skeleton of parser

--- Functions.h
+++ Functions.h
@@ -1,5 +1,19 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct CommandObject {
+    string main_command;
+    vector<string> options, arguments;
+};
+
+queue<string> PipeSeparator(string input_string);
+queue<CommandObject> InputParser(string typed_string);
+
 
 #endif
