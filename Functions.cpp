389f21cfa4066d01a860430b53ff25d251e8c7ef
StephenDHutchings <45437843+StephenDHutchings@users.noreply.github.com>
2020-05-04 15:43:42 -0700

    Add files via upload

--- Functions.cpp
+++ Functions.cpp
@@ -82,24 +82,20 @@ void TheCommands(queue<CommandObject>& commands){
     }
             
     else if(child_pid == 0) {
-   
-        if(a_command.main_command == "/bin/pwd" || a_command.main_command == "/bin/ls"){
-            if(execve(a_command.main_command.c_str(), args, NULL) < 0)
-            {cout << "Could not execute command" << endl;}
+        
+        if(a_command.main_command == "/bin/cd"){
+            chdir(args[1]);
         }
+        
+        else if(execve(a_command.main_command.c_str(), args, NULL) < 0)
+            {cout << "Could not execute command" << endl;}
     }
     
     else {
         wait(NULL);
         return;
     }
-    /*
-    else if(an_object.main_command == "cd"){
-        argument = an_object.arguments[0];
-        args[0] = StringToChar(argument);
-        chdir(args[0]);
-    }
-     */
+
 }
 
 string StartShell(){
@@ -107,8 +103,9 @@ string StartShell(){
     
     cout << "> Welcome to TutorialShell!" << endl;
     cout << "> You may use this Shell as a learning tool by entering the command Tutorial" << endl;
-    cout << "> Otherwise to use this program as a typical Linux shell, enter any other valid command" << endl;
+    cout << "> Otherwise to use this program as a typical Linux shell, enter the command start" << endl;
     cout << "> "; 
+    
     getline(cin, user_input);
     cout << endl;
     return user_input;
@@ -121,6 +118,7 @@ void Shell()
     CommandObject a_command;
     
     cout << "> "; 
+    
     getline(cin, user_input);
     commands = InputParser(user_input);
     a_command = commands.front();
@@ -140,8 +138,13 @@ void TutorialContinue(){
     cout << "> This has implications for other commands that are entered into the shell" << endl;
     cout << "> For example, if a user is interested in running a program or application they must be located in the directory that holds that program" << endl;
     cout << "> To see the current directory, enter the command pwd" << endl << "> ";
+    
+    getline(cin, user_input);
  
-    cin >> user_input;
+    while(user_input != "pwd"){
+        cout << "> Please enter the command pwd" << endl << "> ";
+        getline(cin, user_input);}
+    
      
     if(user_input == "exit")
     {
@@ -158,10 +161,16 @@ void TutorialContinue(){
     cout << "> This command can be modified with different arguments that customize the way the contents of the directory are listed " << endl;
     cout << "> or the path from which to list the files in" << endl;
     cout << "> This tutorial will show the basic command, as well as an example with arguments" << endl;
-    cout << "> To see the contents of the current directory, enter the command ls" << endl << "> ";
+    cout << "> To see the files and subdirectories of the current directory, enter the command ls" << endl << "> ";
     
     user_input = " ";
-    cin >> user_input;
+    
+    getline(cin, user_input);
+    
+    while(user_input != "ls"){
+    cout << "> Please enter the command ls" << endl << "> ";
+    getline(cin, user_input);}
+
        
     if(user_input == "exit")
     {
@@ -169,18 +178,95 @@ void TutorialContinue(){
     }
     
     commands = InputParser(user_input);
-    
     TheCommands(commands);
-    cout << endl;
     
-    cout << "> In addition to these basic commands to see the directory system of the operating system, users are also able to navigate through the directory system" << endl;
+    cout << endl << endl;
+    cout << "> Now we'll add some arguments to the ls command" << endl;
+    cout << "> In a Linux operating system, arguments are used to augment or modify the base command" << endl;
+    cout << "> For the ls command, we'll add the following argument of -l" << endl;
+    cout << "> This will make each file or subdirectory of the current directory be listed on its own line" << endl;
+    cout << "> This will also provide detailed information regarding the contents "  << endl;
+    cout << "> This includes: the unix file type, permissions, the number of hard links, the owner," << endl;
+    cout << "> the group, size, last-modified date, and the filename " << endl;
+    cout << "> The amount of information in these files may be overwhelming - don't focus on that!" << endl;
+    cout << "> The main take away of this is that commands can be modified" << endl;
+    cout << "> Try it out!" << endl;
+    cout << "> Enter the command as: ls -l" << endl;
+    cout << "> ";
+    
+    user_input = " ";
+    
+    getline(cin, user_input);
+    
+    while(user_input != "ls -l"){
+    cout << "> Please enter the command ls -l" << endl << "> ";
+    getline(cin, user_input);}
+
+       
+    if(user_input == "exit")
+    {
+        return;
+    }
+    
+    commands = InputParser(user_input);
+    TheCommands(commands);
+    
+    cout << endl << endl;
+    cout << "> Commands can be given more than one argument, to see the previous command but with the the contents in reverse order" << endl;
+    cout << "> Enter the command: ls -l -r" << endl;
+    cout << "> ";
+    
+    user_input = " ";
+    
+    getline(cin, user_input);
+    
+    while(user_input != "ls -l -r"){
+    cout << "> Please enter the command ls -l -r" << endl << "> ";
+    getline(cin, user_input);}
+
+       
+    if(user_input == "exit")
+    {
+        return;
+    }
+    
+    commands = InputParser(user_input);
+    TheCommands(commands);
+    
+    cout << endl;
+    cout << "> In addition to these basic commands to see the directory system of the operating system" << endl;
+    cout << "> Users are also able to navigate through the directory system" << endl;
     cout << "> The primary command for this is the cd command, which stands for change directory" << endl;
     cout << "> This command takes a valid path in the directory as an argument, and then navigates to the supplied directory" << endl;
     cout << "> To use this command, enter cd followed by a pathway" << endl;
     cout << "> For example: cd /home/stephen/Desktop" << endl;
     cout << "> Try entering this command!" << endl;
+    cout << "> ";
     
-    cin >> user_input;
+    user_input = " ";
+    getline(cin, user_input);
+
+    if(user_input == "exit")
+    {
+        return;
+    }
+    
+    commands = InputParser(user_input);
+    TheCommands(commands);
+    
+    cout << endl;
+    cout << "Now, enter the pwd command to make sure your directory location was successfully changed" << endl;
+    cout << "> ";
+    
+    user_input = " ";
+    getline(cin, user_input);
+    
+    while(user_input != "pwd"){
+    cout << "> Please enter the command pwd" << endl << "> ";
+    getline(cin, user_input);}
+    
+    commands = InputParser(user_input);
+    TheCommands(commands);
     
     return;
 }
@@ -197,14 +283,18 @@ void TutorialStart(){
     cout << "> Please enter the word Start to begin the tutorial" << endl;
     cout << "> If you would like to exit the tutorial, enter the command Exit at anytime" << endl;
     cout << "> ";
-    cin >> user_input;
+    
+    getline(cin, user_input);
+    
     cout << endl;
     
     if (user_input == "start" || user_input == "Start") {
         TutorialContinue();
         }
+    
     else if (user_input == "Exit" || user_input == "exit"){
         return;}
+    
     else{
         cout << "> Command not recognized" << endl;
     }
